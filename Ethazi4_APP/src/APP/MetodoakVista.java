d459f758379c59b961a9ccf5a2762433899189dd
ikerbarrio <iker.barrioel@elorrieta-errekamari.com>
2019-04-10 08:15:35 +0200

    d

--- Ethazi4_APP/src/APP/MetodoakVista.java
+++ Ethazi4_APP/src/APP/MetodoakVista.java
@@ -6,27 +6,33 @@ import VISTA.*;
 
 public class MetodoakVista {
 	public static void bueltatuLehena() {
-		Lehioa1 lehioa1 = new Lehioa1();
-		lehioa1.setVisible(true);
-		lehioa1.setBounds(550, 200, 642, 467);
+		OngiEtorri OngiEtorri = new OngiEtorri();
+		OngiEtorri.setVisible(true);
+		OngiEtorri.setBounds(550, 200, 642, 467);
 }
 
 	public static void bigarrenera() {
-		Lehioa2 lehioa2 = new Lehioa2();
-		lehioa2.setVisible(true);
-		lehioa2.setBounds(550, 200, 642, 467);
+		SaioaHasi SaioaHasi = new SaioaHasi();
+		SaioaHasi.setVisible(true);
+		SaioaHasi.setBounds(550, 200, 642, 467);
 	}
 	
 	public static void hirugarrenera() {
-		Lehioa3 lehioa3 = new Lehioa3();
-		lehioa3.setVisible(true);
-		lehioa3.setBounds(550, 200, 642, 467);
+		hotelHautatu hotelHautatu = new hotelHautatu();
+		hotelHautatu.setVisible(true);
+		hotelHautatu.setBounds(550, 200, 642, 467);
 	}
 	
 	public static void laugarrenera(double prezioFinala) {
-		Lehioa4 lehioa4 = new Lehioa4(prezioFinala);
-		lehioa4.setVisible(true);
-		lehioa4.setBounds(550, 200, 642, 467);
+		Ordainketa Ordainketa = new Ordainketa(prezioFinala);
+		Ordainketa.setVisible(true);
+		Ordainketa.setBounds(550, 200, 642, 467);
+	}
+	
+	public static void saioaSortu() {
+		SaioaSortu SaioaSortu = new SaioaSortu();
+		SaioaSortu.setVisible(true);
+		SaioaSortu.setBounds(550, 200, 642, 467);
 	}
 	
 }
