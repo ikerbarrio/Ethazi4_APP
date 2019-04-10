051dcdd87df61414921472c891c99bc3b5c45584
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-04-10 20:37:47 +0200

    a

--- src/VISTA/SaioaSortu.java
+++ src/VISTA/SaioaSortu.java
@@ -1,27 +1,44 @@
 package VISTA;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerDateModel;
+import javax.swing.SwingConstants;
 
 import APP.MetodoakVista;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import javax.swing.JRadioButton;
 
 public class SaioaSortu extends JFrame {
 
 	private JPanel contentPane;
-	private JButton btnAurrera = new JButton("AURRERA");
 	private ActionListener alBAurrera;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtIzena;
+	private JTextField txtAbizena;
+	private JTextField txtEposta;
+	private JButton btnAurrera = new JButton("AURRERA");
+	private JRadioButton rdbtnGizonezkoa = new JRadioButton("GIZONA");
 	private JRadioButton rdbtnEmakumea = new JRadioButton("EMAKUMEA");
+	private JLabel lblSaioaSortu = new JLabel("SAIOA SORTU");
+	private JLabel lblAbizena = new JLabel("Abizena");
+	private JLabel lblIzena = new JLabel("Izena");
+	private final JLabel lblPasahitza = new JLabel("Pasahitza");
+	private JLabel lblEposta = new JLabel("E-posta");
+	private final JLabel lblErabiltzailea = new JLabel("Erabiltzailea");
+	private final JSpinner jaioData = new JSpinner();
+	private JPasswordField pasahitza;
+	private final JLabel lblJaiotze = new JLabel("Jaiotze data");
+
 	/**
 	 * Create the frame.
 	 */
@@ -34,43 +51,59 @@ public class SaioaSortu extends JFrame {
 		btnAurrera.setBounds(520, 393, 89, 23);
 		getContentPane().add(btnAurrera);
 
-		textField = new JTextField();
-		textField.setBounds(95, 72, 182, 36);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		txtIzena = new JTextField();
+		txtIzena.setBounds(121, 57, 182, 36);
+		getContentPane().add(txtIzena);
+		txtIzena.setColumns(10);
 
-		JLabel lblSaioaSortu = new JLabel("SAIOA SORTU");
 		lblSaioaSortu.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSaioaSortu.setFont(new Font("Tahoma", Font.BOLD, 22));
 		lblSaioaSortu.setBounds(230, 10, 160, 36);
 		getContentPane().add(lblSaioaSortu);
 
-		JLabel lblIzena = new JLabel("IZENA");
 		lblIzena.setHorizontalAlignment(SwingConstants.CENTER);
 		lblIzena.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblIzena.setBounds(10, 70, 75, 36);
+		lblIzena.setBounds(27, 57, 75, 36);
 		getContentPane().add(lblIzena);
 
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(95, 132, 182, 36);
-		getContentPane().add(textField_1);
+		txtAbizena = new JTextField();
+		txtAbizena.setColumns(10);
+		txtAbizena.setBounds(121, 104, 182, 36);
+		getContentPane().add(txtAbizena);
 
-		JLabel lblAbizena = new JLabel("ABIZENA");
 		lblAbizena.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAbizena.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblAbizena.setBounds(10, 132, 75, 36);
+		lblAbizena.setBounds(27, 104, 75, 36);
 		getContentPane().add(lblAbizena);
 
-		JRadioButton rdbtnGizonezkoa = new JRadioButton("GIZONA");
 		rdbtnGizonezkoa.setHorizontalAlignment(SwingConstants.CENTER);
-		rdbtnGizonezkoa.setBounds(10, 203, 109, 23);
+		rdbtnGizonezkoa.setBounds(15, 352, 109, 23);
 		getContentPane().add(rdbtnGizonezkoa);
 
-		
 		rdbtnEmakumea.setHorizontalAlignment(SwingConstants.CENTER);
-		rdbtnEmakumea.setBounds(121, 203, 109, 23);
+		rdbtnEmakumea.setBounds(173, 352, 109, 23);
 		getContentPane().add(rdbtnEmakumea);
+
+		txtEposta = new JTextField();
+		txtEposta.setBounds(121, 151, 182, 36);
+		getContentPane().add(txtEposta);
+		txtEposta.setColumns(10);
+
+		lblEposta.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblEposta.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEposta.setBounds(27, 151, 75, 36);
+		getContentPane().add(lblEposta);
+
+		lblErabiltzailea.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblErabiltzailea.setHorizontalAlignment(SwingConstants.CENTER);
+		lblErabiltzailea.setBounds(15, 198, 100, 36);
+
+		getContentPane().add(lblErabiltzailea);
+		lblPasahitza.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblPasahitza.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPasahitza.setBounds(15, 245, 100, 41);
+
+		getContentPane().add(lblPasahitza);
 
 		alBAurrera = new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -81,5 +114,21 @@ public class SaioaSortu extends JFrame {
 			};
 		};
 		btnAurrera.addActionListener(alBAurrera);
+
+		jaioData.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+		jaioData.setModel(new SpinnerDateModel(new Date(1546297200000L), null, null, Calendar.DAY_OF_YEAR));
+		jaioData.setBounds(121, 292, 182, 36);
+
+		getContentPane().add(jaioData);
+
+		pasahitza = new JPasswordField();
+		pasahitza.setBounds(121, 245, 182, 36);
+		getContentPane().add(pasahitza);
+		lblJaiotze.setHorizontalAlignment(SwingConstants.CENTER);
+		lblJaiotze.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblJaiotze.setBounds(15, 292, 100, 36);
+		
+		getContentPane().add(lblJaiotze);
 	}
 }
