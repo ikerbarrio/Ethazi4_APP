6bc370d968cb3d92498663f5968727f2d5d5542a
Rreguero <gorka.reguerohe@elorrieta-errekamari.com>
2019-04-14 16:57:22 +0200

    aa

--- src/APP/Main.java
+++ src/APP/Main.java
@@ -16,7 +16,7 @@ public class Main {
 		Konexioa con1 = new Konexioa(basedatos);
 		con1.getConexion();
 		
-		System.out.println("penepene");
+
 
 		VISTA.OngiEtorri ejecutar = new VISTA.OngiEtorri();
 		ejecutar.setVisible(true);
