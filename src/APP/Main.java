299d9fe9aef8111447941c4ed67c478383863d82
Rreguero <gorka.reguerohe@elorrieta-errekamari.com>
2019-04-24 20:20:30 +0200

    lo mio

--- src/APP/Main.java
+++ src/APP/Main.java
@@ -15,7 +15,7 @@ System.out.println("sdsdsdsd");
 		Konexioa con1 = new Konexioa(basedatos);
 		con1.getConexion();
 
-
+System.out.println();
 
 		VISTA.OngiEtorri ejecutar = new VISTA.OngiEtorri();
 		ejecutar.setVisible(true);
