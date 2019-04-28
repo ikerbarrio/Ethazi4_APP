dccbd67277f7d3d4c017ffaa7e790a6798281f22
Rreguero <gorka.reguerohe@elorrieta-errekamari.com>
2019-04-28 12:46:02 +0200

    commit

--- src/APP/Main.java
+++ src/APP/Main.java
@@ -10,12 +10,12 @@ public class Main {
 		 
 		Metodoak m = new Metodoak();
 
-System.out.println("sdsdsdsd");
+
 		String basedatos = "ethazi4";
 		Konexioa con1 = new Konexioa(basedatos);
 		con1.getConexion();
 
-System.out.println();
+
 
 		VISTA.OngiEtorri ejecutar = new VISTA.OngiEtorri();
 		ejecutar.setVisible(true);
