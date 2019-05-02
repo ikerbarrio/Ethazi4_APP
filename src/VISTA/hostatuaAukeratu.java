5de6693ede4189d49ad8bf3be6483311a342ffbf
ikerbarrio <iker.barrioel@elorrieta-errekamari.com>
2019-05-02 14:17:14 +0200

    d

---
+++ src/VISTA/hostatuaAukeratu.java
@@ -0,0 +1,28 @@
+package VISTA;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class hostatuaAukeratu extends JFrame {
+
+	private JPanel contentPane;
+
+
+	public hostatuaAukeratu() {
+		setForeground(Color.DARK_GRAY);
+		getContentPane().setForeground(Color.DARK_GRAY);
+		this.setSize(478, 300);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(550, 200, 635, 455);
+		getContentPane().setLayout(null);
+		
+		
+	}
+
+}
